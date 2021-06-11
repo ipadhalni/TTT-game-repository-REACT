d88e632eb6af193c31ec6a35cf2847200b49c264
ipadhalni <sachinpadhalni9@gmail.com>
2021-06-12 00:11:37 +0530

    My first project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+//import './components/overview';
+
+import Game from './components/Game';
+
+
+function App(){
+
+  return(
+    <div className="App">
+      <Game />
+
+    </div>
+
+  );
+}
+
+export default App;
